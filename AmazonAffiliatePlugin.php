0ceb502e95701a4f9c01cfc86b2d1536127c558c
Denis Komlev <heavytalks@gmail.com>
2014-06-22 19:05:06 +0400

    Fix tabs

--- AmazonAffiliatePlugin.php
+++ AmazonAffiliatePlugin.php
@@ -1,3 +1,4 @@
+//<?php
 if ($modx->event->name == 'OnWebPagePrerender')
 {
     $content = $modx->documentOutput;
@@ -15,35 +16,35 @@ if ($modx->event->name == 'OnWebPagePrerender')
             // Get and parse the value of "href" attribute
             $url = trim($matches[1][$key]);
             $url_info = parse_url($url);
-			
-			// Skip non-amazon urls
-			$host = isset($url_info['host']) ? strtolower($url_info['host']) : '';
-			if (!in_array($host, array('amazon.com', 'www.amazon.com'))) {
-				continue;
-			}
-			
-			// Skip links which already have Associates ID
-			if (isset($url_info['query'])) {
-				$url_info['query'] = str_replace('&amp;', '&', $url_info['query']);
-				parse_str($url_info['query'], $query);
-				if (!empty($query['tag'])) {
-					continue;
-				}
-			}
+            
+            // Skip non-amazon urls
+            $host = isset($url_info['host']) ? strtolower($url_info['host']) : '';
+            if (!in_array($host, array('amazon.com', 'www.amazon.com'))) {
+                continue;
+            }
+            
+            // Skip links which already have Associates ID
+            if (isset($url_info['query'])) {
+                $url_info['query'] = str_replace('&amp;', '&', $url_info['query']);
+                parse_str($url_info['query'], $query);
+                if (!empty($query['tag'])) {
+                    continue;
+                }
+            }
 
             // Add Associates ID to the link
-			$new_url = $url_info['scheme'] . '://' . $url_info['host'];
-			if (isset($url_info['path'])) {
-				$new_url .= $url_info['path'];
-			}
-			$new_url = rtrim($new_url, '/') . '/?tag=' . $affiliate_id;
-			if (isset($url_info['query'])) {
-				$new_url .= '&' . $url_info['query'];
-			}
-			if (isset($url_info['fragment'])) {
-				$new_url .= '#' . $url_info['fragment'];
-			}
-			$new_tag = preg_replace("/href=[\"\'](.*?)[\"\']/", 'href="' . $new_url . '"', $tag);
+            $new_url = $url_info['scheme'] . '://' . $url_info['host'];
+            if (isset($url_info['path'])) {
+                $new_url .= $url_info['path'];
+            }
+            $new_url = rtrim($new_url, '/') . '/?tag=' . $affiliate_id;
+            if (isset($url_info['query'])) {
+                $new_url .= '&' . $url_info['query'];
+            }
+            if (isset($url_info['fragment'])) {
+                $new_url .= '#' . $url_info['fragment'];
+            }
+            $new_tag = preg_replace("/href=[\"\'](.*?)[\"\']/", 'href="' . $new_url . '"', $tag);
             $content = str_replace($tag, $new_tag, $content);
         }
 
